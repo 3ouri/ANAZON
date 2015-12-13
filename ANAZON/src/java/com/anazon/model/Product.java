e6e17e23e42873701fcfbdea7bc8923e99125ffc
3ouri <abdallah.ouri@hotmail.com>
2015-12-13 17:47:16 -0600

    initial commit

---
+++ ANAZON/src/java/com/anazon/model/Product.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.anazon.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Abdallah
+ */
+@Entity
+@Table(name = "product")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p")})
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 45)
+    @Column(name = "name")
+    private String name;
+    @Size(max = 250)
+    @Column(name = "descrption")
+    private String descrption;
+    @Column(name = "qunitity")
+    private Integer qunitity;
+    @Lob
+    @Column(name = "image")
+    private byte[] image;
+    @Column(name = "price")
+    private Integer price;
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    @ManyToOne
+    private ProductCategory categoryId;
+    @OneToMany(mappedBy = "productId")
+    private List<Orders> ordersList;
+
+    public Product() {
+    }
+
+    public Product(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescrption() {
+        return descrption;
+    }
+
+    public void setDescrption(String descrption) {
+        this.descrption = descrption;
+    }
+
+    public Integer getQunitity() {
+        return qunitity;
+    }
+
+    public void setQunitity(Integer qunitity) {
+        this.qunitity = qunitity;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public ProductCategory getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(ProductCategory categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @XmlTransient
+    public List<Orders> getOrdersList() {
+        return ordersList;
+    }
+
+    public void setOrdersList(List<Orders> ordersList) {
+        this.ordersList = ordersList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.anazon.controller.Product[ id=" + id + " ]";
+    }
+    
+}
