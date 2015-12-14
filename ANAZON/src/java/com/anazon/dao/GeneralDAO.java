5d12de481e3c9bfbe25aad618110dc60a16e4d7e
3ouri <abdallah.ouri@hotmail.com>
2015-12-14 17:05:48 -0600

    Added new classes

---
+++ ANAZON/src/java/com/anazon/dao/GeneralDAO.java
@@ -0,0 +1,150 @@
+package com.anazon.dao;
+
+import com.anazon.common.ConfigurationConstants.SortingType;
+import java.util.Collection;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.eclipse.persistence.expressions.Expression;
+import org.eclipse.persistence.jpa.JpaEntityManager;
+import org.eclipse.persistence.queries.ReadAllQuery;
+import org.eclipse.persistence.queries.SQLCall;
+import org.eclipse.persistence.sessions.UnitOfWork;
+
+public class GeneralDAO {
+
+    private static GeneralDAO generalDAO;
+    private static final String persistenceUnitName = "DataLayer";
+    private static EntityManagerFactory factory;
+    private JpaEntityManager em;
+    private UnitOfWork uow;
+
+    private GeneralDAO() {
+        factory = Persistence.createEntityManagerFactory(persistenceUnitName);
+    }
+
+    public static GeneralDAO getInstance() {
+        if (generalDAO == null) {
+            return new GeneralDAO();
+        }
+        return generalDAO;
+    }
+
+    private void instantiateConnection() {
+        em = (JpaEntityManager) factory.createEntityManager();
+        em.getTransaction().begin();
+        uow = em.getUnitOfWork();
+    }
+
+    private void terminateConnection() {
+        em.getTransaction().commit();
+        uow.release();
+        em.close();
+    }
+
+    private ReadAllQuery createReadAllQuery(Class clazz, Expression expression,
+            SortingType sortingType, String columnName) {
+        ReadAllQuery query = new ReadAllQuery();
+        query.setReferenceClass(clazz);
+        query.setSelectionCriteria(expression);
+        if (sortingType.equals(SortingType.ASCENDING)) {
+            query.addAscendingOrdering(columnName);
+        } else {
+            query.addDescendingOrdering(columnName);
+        }
+        return query;
+    }
+
+    public <T> T getAllObjects(Class clazz) {
+        instantiateConnection();
+        T clone;
+        clone = (T) uow.readAllObjects(clazz);
+        terminateConnection();
+        return clone;
+    }
+
+    public <T> T getAllObjectsByExpression(Class clazz, Expression expression) {
+        instantiateConnection();
+        T clone;
+        clone = (T) uow.readAllObjects(clazz, expression);
+        terminateConnection();
+        return clone;
+    }
+
+    public <T> T getAllObjectsByExpression(Class clazz, Expression expression,
+            SortingType sortingType, String columnName) {
+        instantiateConnection();
+        ReadAllQuery query = createReadAllQuery(clazz, expression, sortingType,
+                columnName);
+
+        T clone;
+        clone = (T) uow.executeQuery(query);
+        terminateConnection();
+        return clone;
+    }
+
+    public <T> T getObjectByExpression(Class clazz, Expression expression) {
+        instantiateConnection();
+
+        T clone;
+        clone = (T) uow.readObject(clazz, expression);
+        terminateConnection();
+        return clone;
+    }
+
+    public <T> T addObject(T t) {
+        instantiateConnection();
+        T clone;
+        clone = (T) uow.registerObject(t);
+        // System.out.println(clone.toString());
+        terminateConnection();
+        return clone;
+    }
+
+    public <T> T updateObject(T t) {
+        instantiateConnection();
+        T clone;
+        clone = (T) uow.deepMergeClone(t);
+        terminateConnection();
+        return clone;
+    }
+
+    public int executeNonSelectingSQLCall(String sql) {
+        instantiateConnection();
+        try {
+            uow.executeNonSelectingCall(new SQLCall(sql));
+            return 1;// Success
+
+        } catch (Exception ex) {
+            System.out
+                    .println(" GeneralDAO.executeNonSelectingSQLCall catch msg "
+                            + ex.getMessage());
+            return -1;// fail
+
+        } finally {
+            System.out.println(" terminate Connection ");
+            terminateConnection();
+        }
+    }
+
+    public <T> T deleteObject(T t) {
+        instantiateConnection();
+        T clone;
+        clone = (T) uow.deleteObject(t);
+        terminateConnection();
+        return clone;
+    }
+
+    public <T> void deleteAllObjects(T objects) {
+        instantiateConnection();
+        uow.deleteAllObjects((Collection) objects);
+        terminateConnection();
+    }
+
+    public <T> T deleteAllObjectsBasedOnExpression(Class clazz,
+            Expression expression) {
+        T clone;
+        clone = getAllObjectsByExpression(clazz, expression);
+        deleteAllObjects(clone);
+        return clone;
+    }
+}
